f5c5a9281f2c6bc4b2b150c876e219d92eefe02e
Alex Oberhofer <alexmoberhofer@gmail.com>
2020-07-13 14:58:37 -0400

    Clones one repository

--- src/git-backup.go
+++ src/git-backup.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/go-git/go-git"
 )
+
+var repositories [10]string
 
 func CreateDirIfNotExist(dir string) {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
@@ -16,11 +19,26 @@ func CreateDirIfNotExist(dir string) {
 	}
 }
 
+func Usage() {
+	fmt.Printf("Usage: \n")
+}
+
+func GetCurrentTimeStamp() string {
+	return time.Now().Format(time.RFC850)
+}
+
 func main() {
 	url := "https://github.com/AlexOberhofer/SDL2-GNUBoy.git"
 	directory := "./backup"
 
+	time := GetCurrentTimeStamp()
+
 	CreateDirIfNotExist(directory)
+
+	fmt.Printf(time)
+	fmt.Printf("\n")
+
+	fmt.Printf("Attempting to clone repository: %s\n", url)
 
 	git.PlainClone(directory, false, &git.CloneOptions{
 		URL:               url,
