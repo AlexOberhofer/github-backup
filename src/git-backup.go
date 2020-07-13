8abcda1712cf8a8a17e563a8869d03bb3b0521ea
Alex Oberhofer <alexmoberhofer@gmail.com>
2020-07-13 15:13:09 -0400

    add more utility to script

--- src/git-backup.go
+++ src/git-backup.go
@@ -8,7 +8,28 @@ import (
 	"github.com/go-git/go-git"
 )
 
-var repositories [10]string
+var repositories [5]string = [5]string{
+	"https://github.com/AlexOberhofer/SDL2-GNUBoy.git",
+	"https://github.com/AlexOberhofer/Space-Invaders.git",
+	"https://github.com/AlexOberhofer/Chip-8-Emulator.git",
+	"https://github.com/AlexOberhofer/pylife.git",
+	"https://github.com/AlexOberhofer/sdl2-doom.git"}
+
+func ListRepositories() {
+	for _, element := range repositories {
+		fmt.Printf(element)
+		fmt.Printf("\n")
+	}
+}
+
+func DoAllClones(url string, directory string) {
+	fmt.Printf("Attempting to clone repository: %s\n", url)
+
+	git.PlainClone(directory, false, &git.CloneOptions{
+		URL:               url,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	})
+}
 
 func CreateDirIfNotExist(dir string) {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
@@ -31,6 +52,10 @@ func main() {
 	url := "https://github.com/AlexOberhofer/SDL2-GNUBoy.git"
 	directory := "./backup"
 
+	//fmt.Printf("The following repositories will be cloned: \n")
+
+	//ListRepositories()
+
 	time := GetCurrentTimeStamp()
 
 	CreateDirIfNotExist(directory)
@@ -40,10 +65,10 @@ func main() {
 
 	fmt.Printf("Attempting to clone repository: %s\n", url)
 
-	git.PlainClone(directory, false, &git.CloneOptions{
-		URL:               url,
-		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
-	})
+	//git.PlainClone(directory, false, &git.CloneOptions{
+	//URL:               url,
+	//	RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	//})
 
 	fmt.Printf("Completed!\n")
 }
