b7f9f11cc104da7ba2000ffe911f52529b1a284a
Alex Oberhofer <alexmoberhofer@gmail.com>
2020-07-17 10:19:43 -0400

    backup can be tarred

--- git-backup/file-operations.go
+++ git-backup/file-operations.go
@@ -19,10 +19,14 @@
 package main
 
 import (
+	"archive/tar"
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 // CreateDirIfNotExist -
@@ -64,4 +68,71 @@ func DeleteBackupFromDir(dir string) {
 		os.Exit(1)
 	}
 
+}
+
+// Tar -
+//
+// Creates a tar file from the backup directory
+////////////////////////////////////////////////////////////////////////
+func Tar(src string, writers ...io.Writer) error {
+
+	// ensure the src actually exists before trying to tar it
+	if _, err := os.Stat(src); err != nil {
+		return fmt.Errorf("Unable to tar files - %v", err.Error())
+	}
+
+	mw := io.MultiWriter(writers...)
+
+	tw := tar.NewWriter(mw)
+	defer tw.Close()
+
+	// walk path
+	return filepath.Walk(src, func(file string, fi os.FileInfo, err error) error {
+
+		// return on any error
+		if err != nil {
+			return err
+		}
+
+		// return on non-regular files (thanks to [kumo](https://medium.com/@komuw/just-like-you-did-fbdd7df829d3) for this suggested update)
+		if !fi.Mode().IsRegular() {
+			return nil
+		}
+
+		/* Since we are creating a backup and not active clones - I am electing to ignore the .git folder */
+		if strings.Contains(file, ".git") {
+			return nil
+		}
+
+		// create a new dir/file header
+		header, err := tar.FileInfoHeader(fi, fi.Name())
+		if err != nil {
+			return err
+		}
+
+		// update the name to correctly reflect the desired destination when untaring
+		header.Name = strings.TrimPrefix(strings.Replace(file, src, "", -1), string(filepath.Separator))
+
+		// write the header
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		// open files for taring
+		f, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+
+		// copy file data into tar writer
+		if _, err := io.Copy(tw, f); err != nil {
+			return err
+		}
+
+		// manually close here after each file operation; defering would cause each file close
+		// to wait until all operations have completed.
+		f.Close()
+
+		return nil
+	})
 }
--- git-backup/git-backup.go
+++ git-backup/git-backup.go
@@ -21,6 +21,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -40,6 +41,7 @@ func main() {
 	username := flag.String("q", "", "Github Username to query")
 	url := flag.String("cs", "", "Single Repository URL to clone")
 	userPublicClone := flag.String("cu", "", "Clone all public repositories for GitHub user")
+	userPublicCloneAndTar := flag.String("cut", "", "Clone and Tar all public repositories for GitHub user")
 	removeBackupFromDir := flag.String("r", "", "Remove backup directory")
 	flag.Parse()
 
@@ -58,6 +60,21 @@ func main() {
 
 	if *userPublicClone != "" {
 		CloneAllPublicRepos(*userPublicClone)
+	}
+
+	if *userPublicCloneAndTar != "" {
+		CloneAllPublicRepos(*userPublicCloneAndTar)
+		writer, writerErr := os.Create("./backup.tar")
+
+		if writerErr != nil {
+			panic(writerErr)
+		}
+
+		err := Tar("./backup", writer)
+
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	if *removeBackupFromDir != "" {
